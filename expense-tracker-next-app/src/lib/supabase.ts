f8ec2ca35dad2f44b6aa5e231b912e4549a13360
waleedejaz9 <waleedejaz9@gmail.com>
2025-02-24 08:05:46 +0500

    fixes env keys issue

--- expense-tracker-next-app/src/lib/supabase.ts
+++ expense-tracker-next-app/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://gwhgtpftatbeizlhcfzy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3aGd0cGZ0YXRiZWl6bGhjZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwOTIyODQsImV4cCI6MjA1NTY2ODI4NH0.P0bMEVO_S840OUEYG5C3HITwXicCnWPZvSNpanuTU_o';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
